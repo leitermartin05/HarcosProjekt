e3f35686d66fa55376c64feae93577ca0d4837d4
leitermartin05 <martosm05@gmail.com>
2020-09-25 10:45:04 +0200

    Változók, konstruktorok, getter-szetterek létrehozása

--- Harcos.cs
+++ Harcos.cs
@@ -8,5 +8,43 @@ namespace HarcosProjekt
 {
     class Harcos
     {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
+
+        public Harcos(string nev, int statuszSablon)
+        {
+            this.nev = nev;
+            this.szint = 1;
+            this.tapasztalat = 0;
+            switch (statuszSablon)
+            {
+                case 1:
+                    this.alapEletero = 15;
+                    this.alapSebzes = 3; break;
+                case 2:
+                    this.alapEletero = 12;
+                    this.alapSebzes = 4; break;
+                case 3:
+                    this.alapEletero = 8;
+                    this.alapSebzes = 5; break;
+                default: break;
+            }
+            this.eletero = MaxEletero;
+            
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int AlapEletero { get => alapEletero; }
+        public int AlapSebzes { get => alapSebzes; }
+        public int Sebzes { get => alapSebzes + szint; }
+        public int SzintLepeshez { get => 10 + szint * 5; }
+        public int MaxEletero { get => alapEletero + szint * 3; }
     }
 }
