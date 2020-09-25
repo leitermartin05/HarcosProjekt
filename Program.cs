894e00230b6b3982db463ecd166baf5d8610c1f7
leitermartin05 <martosm05@gmail.com>
2020-09-25 12:44:27 +0200

    StreamReader létrehozása, beolvasás elkezdése

--- Program.cs
+++ Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HarcosProjekt
 {
     class Program
     {
+        StreamReader sr = new StreamReader("harcosok.csv");
+        List<Harcos> lista = new List<Harcos>();
         static void Main(string[] args)
         {
+            
+            
         }
+
+        
     }
 }
